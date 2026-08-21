09903fbc5924f613c0cb30d6cc7e7aaa194adbab
salonichhabra27 <salonichhabra27@gmail.com>
2026-08-21 20:37:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import numpy as np
+import matplotlib.pyplot as plt
 
 # -------------------------------------------------
 # PAGE CONFIGURATION
